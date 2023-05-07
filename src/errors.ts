6d07a7aa87c9a0c1254286f9aa78bf6d8abb059b
dovh-me <dovh.me@gmail.com>
2023-05-07 22:34:45 +0530

    [important bug fix] initialize name properties within the constructors of error classes

--- src/errors.ts
+++ src/errors.ts
@@ -3,24 +3,24 @@ import * as types from './types';
 export class JsonParseError extends Error {
     err: Error;
     filePath: string;
-    name = 'JsonParseError';
 
     constructor(err: unknown, filePath: string) {
         const parsedError = isError(err) ? err : new Error('unknown error');
-
         super(parsedError.message);
+
         this.err = parsedError;
         this.filePath = filePath;
+        this.name = 'JsonParseError';
     }
 }
 
 export class ConfigJsonValidateError extends Error {
     filePath: string;
     messages: string[][];
-    name = 'ConfigJsonValidateError';
 
     constructor(messages: string[][], filePath: string) {
         super();
+        this.name = 'ConfigJsonValidateError';
         this.messages = messages;
         this.filePath = filePath;
     }
@@ -29,10 +29,10 @@ export class ConfigJsonValidateError extends Error {
 export class FailedRuleError extends Error {
     paths: (string | RegExp)[];
     rule: types.FileRule | types.DirectoryRule;
-    name = 'FailedRuleError';
 
     constructor(rule: types.FileRule | types.DirectoryRule, paths: (string | RegExp)[]) {
         super(`${JSON.stringify(rule)}, deep: ${paths.length}, rule did not passed`);
+        this.name = 'FailedRuleError';
         this.rule = rule;
         this.paths = paths;
     }
@@ -40,20 +40,20 @@ export class FailedRuleError extends Error {
 
 export class InvalidPathError extends Error {
     path: string;
-    name = 'InvalidPathError';
 
     constructor(path: string) {
         super(`${path}, was not validated due to path being invalid`);
+        this.name = 'InvalidPathError';
         this.path = path;
     }
 }
 
 export class ValidationError extends Error {
     errors: Error[];
-    name = 'ValidationError';
 
     constructor(errors: Error[]) {
         super('There were errors validating the project');
+        this.name = 'ValidationError';
         this.errors = errors;
     }
 }
