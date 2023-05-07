c3782a468e530dc07c241f5feb30ed13be1c900a
dovh-me <dovh.me@gmail.com>
2023-05-07 20:25:04 +0530

    expose errors in the api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import * as validator from '../validator';
 import * as program from '../program';
+import * as _errors from '../errors';
 
 export const validate = validator.run;
 export const getProgramData = program.run;
+export const errors = _errors;
